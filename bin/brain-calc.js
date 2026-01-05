46c574633b7e030bb610398e616da1bb59e4285f
Dmitry-ekb <ekbicp@gmail.com>
2026-01-05 23:07:57 +0500

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import runGame from '../src/engine.js'
+import runGame from '../src/index.js'
 import { rules, getQuestionAndAnswer } from '../src/games/calc.js'
 
 runGame(rules, getQuestionAndAnswer)
